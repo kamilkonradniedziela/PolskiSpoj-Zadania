9a7164989a32bf8e4a6f7fe7c60017b62b0dd1aa
Kamil Niedziela <56163722+kaminie318@users.noreply.github.com>
2020-07-07 17:18:00 +0200

    Add files via upload

---
+++ SzyfrCezara.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string napis;
+    while(getline(cin,napis))
+    {
+        for(int i = 0;i<napis.length();i++)
+        {
+            if(napis[i]>87)
+            {
+                if(napis[i]=='X')
+                napis[i]=='A';
+                if(napis[i]=='Y')
+                napis[i]=='B';
+                if(napis[i]=='Z')
+                napis[i]=='C';
+            }
+            else if(napis[i]>64)
+            {
+                napis[i]=napis[i]+3;
+            }
+        }
+
+        for(int i = 0;i<napis.length();i++)
+        {
+            cout<<napis[i];
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
