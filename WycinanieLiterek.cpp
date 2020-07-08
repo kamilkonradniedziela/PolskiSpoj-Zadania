63e44ec4d071e1c0cbebce3c3792d2c5ba306cb7
Kamil Niedziela <56163722+kaminie318@users.noreply.github.com>
2020-07-08 23:44:29 +0200

    Add files via upload

---
+++ WycinanieLiterek.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string wyraz;
+    char litera;
+    while(cin>>litera)
+    {
+        cin>>wyraz;
+        for(int i = 0;i<wyraz.length();i++)
+        {
+            if(wyraz[i]!=litera)
+            {
+                cout<<wyraz[i];
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
